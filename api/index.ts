062b85e44f8df92ec2f6cd181e3043c489f0cf6e
xosaad <xosaad@users.noreply.github.com>
2023-06-01 11:59:31 +0400

    serverless

--- api/index.ts
+++ api/index.ts
@@ -1,39 +1,40 @@
-import express, {Application, Request, Response} from "express"
-import cors from "cors"
-import {Configuration, OpenAIApi} from 'openai'
-import * as dotenv from "dotenv"
-dotenv.config()
-const PORT: number = 8000
-const app: Application = express()
+import express, { Request, Response } from 'express';
+import { Configuration, OpenAIApi } from 'openai';
+import serverless from 'serverless-http';
+import cors from 'cors';
 
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-const API_KEY: string = process.env.API_KEY as string
+const API_KEY: string = process.env.API_KEY as string;
 
 const configuration = new Configuration({
-    apiKey:API_KEY
-})
+  apiKey: API_KEY,
+});
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
-app.post("/api/completions", async (req: Request, res: Response) => {
-    try{
-        const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{
-                            role: "user" , 
-                            content: "Return ONLY SQL for" + req.body.message
-                        }]
-        })
-        console.log(completion)
-        res.send(completion.data.choices[0].message)
-    }
-    catch(error){
-        console.error(error)
-        res.status(500).send("Server Error")
-    }
-})
+app.post('/completions', async (req: Request, res: Response) => {
+  try {
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: 'Return ONLY SQL for ' + req.body.message,
+        },
+      ],
+    });
+    console.log(completion);
+    res.send(completion.data.choices[0].message);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
 
-app.listen(PORT, () => console.log(`your server is running on PORT ${PORT}`))
+const handler = serverless(app);
+
+export { handler };
