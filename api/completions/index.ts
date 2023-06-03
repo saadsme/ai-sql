b08eb66c76b582d467a85c660e7dfa8cfdac2c0b
xosaad <xosaad@users.noreply.github.com>
2023-06-03 14:24:17 +0400

    try2

--- api/completions/index.ts
+++ api/completions/index.ts
@@ -1,12 +1,8 @@
 import express, { Request, Response } from 'express';
 import { Configuration, OpenAIApi } from 'openai';
-import serverless from 'serverless-http';
-import cors from 'cors';
 
-const app = express();
 
-app.use(express.json());
-app.use(cors());
+
 
 const API_KEY: string = process.env.API_KEY as string;
 
@@ -16,7 +12,8 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-app.post('/completions', async (req: Request, res: Response) => {
+
+export async function POST(req :Request, res: Response) {
   try {
     const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
@@ -28,13 +25,9 @@ app.post('/completions', async (req: Request, res: Response) => {
       ],
     });
     console.log(completion);
-    res.send(completion.data.choices[0].message);
+    res.send(JSON.stringify(completion.data.choices[0].message));
   } catch (error) {
     console.error(error);
-    res.status(500).send('Server Error');
+    return res.status(500).send('Server Error');
   }
-});
-
-const handler = serverless(app);
-
-export { handler };
+}
